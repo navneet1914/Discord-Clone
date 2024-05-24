ec742c5df790eb41be77bcd80e6db9d1dc58a8df
Navneet <navneetkumar142000@gmail.com>
2024-05-24 22:13:49 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"],
+      },
+      
+      colors: {
+      deepPurple: "#404EED",
+    },
+  },
+  },
+  plugins: [],
+  
+}
+
